afe8f299a55b87ca94e2d415de5b7b964378fb33
ArthackA <bsclmr650118@spu.ac.ke>
2020-01-22 12:49:19 +0300

    update

--- scrapper101.py
+++ scrapper101.py
@@ -2,15 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.brightermonday.co.ke/jobs/"
-response = requests.get(url)
-
-
-data = response.text
-
-
-soup = BeautifulSoup(data,'html.parser')
-
-titles = soup.find_all('a',{'class':'search-result__job-title metrics-apply-now'})
-
-for title in titles:
-    print(title.text)
+job_no = 0
+while True:
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data,'html.parser')
+    jobs = soup.find_all('article',{'class':'search-result'})
+    
+    for job in jobs:
+        title = job.find('a',{'class':'search-result__job-title metrics-apply-now'}).text
+        #company = job.find('div',{'class': 'search-result__body'})
+        #JobFunc = job.find('div',{'class': 'search-result__job-function'})
+        print('Job Title: ',title)
+        job_no+=1
+    url_tag = soup.find('li',{'class':'page-item'})
+    if url_tag.get('href'):
+        url = 'https://www.brightermonday.co.ke'+ url_tag.get('href')
+        print(url)
+    else:
+        break
+    
+print("Total Number Of Jobs: ",job_no)
